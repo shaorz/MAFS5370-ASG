8c846790c7c6dfb650fd28ca85917514d3eccc19
Roger SHAO <shaorz@gmail.com>
2023-05-08 21:21:19 +0800

    MAFS-0002 initial commit for assignment 2

--- GACA_GBM.py
+++ GACA_GBM.py
@@ -1,128 +1,136 @@
 import numpy as np
-import tensorflow as tf
-import tensorflow_probability as tfp
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
 
 # Set the option parameters
-S0 = 100   # Stock price
-K = 100    # Strike price
-T = 10     # Time to maturity (in years)
-sigma = 0.2   # Volatility
-r = 0.05   # Risk-free rate
+S0 = 100  # Stock price
+K = 100  # Strike price
+T = 10  # Time to maturity (in years)
+sigma = 0.2  # Volatility
+r = 0.05  # Risk-free rate
 
-# Define the Black-Scholes model for European put option pricing
-def bs_put(S, K, T, r, sigma):
-    d1 = (np.log(S/K) + (r + sigma**2/2)*T) / (sigma*np.sqrt(T))
-    d2 = d1 - sigma*np.sqrt(T)
-    Nd1 = tfp.distributions.Normal(0, 1).cdf(-d1)
-    Nd2 = tfp.distributions.Normal(0, 1).cdf(-d2)
-    return K*tf.math.exp(-r*T)*Nd2 - S*Nd1
 
-# Define the state and action spaces
-state_dim = 1  # Stock price
-action_dim = 1  # Exercise decision (0 for hold, 1 for exercise)
+class Actor ( nn.Module ):
+	def __init__ ( self , state_dim , action_dim ):
+		super ( Actor , self ).__init__ ()
+		self.fc1 = nn.Linear ( state_dim , 128 )
+		self.fc2 = nn.Linear ( 128 , 64 )
+		self.fc3 = nn.Linear ( 64 , action_dim )
 
-# Define the actor and critic networks
-def create_actor_network(state_dim, action_dim):
-    state_input = Input(shape=(state_dim,))
-    x = Dense(32, activation='relu')(state_input)
-    x = Dense(32, activation='relu')(x)
-    action_output = Dense(action_dim, activation='sigmoid')(x)
-    model = Model(inputs=state_input, outputs=action_output)
-    return model
+	def forward ( self , state ):
+		x = F.relu ( self.fc1 ( state ) )
+		x = F.relu ( self.fc2 ( x ) )
+		x = torch.tanh ( self.fc3 ( x ) )
+		return x
 
-def create_critic_network(state_dim):
-    state_input = Input(shape=(state_dim,))
-    x = Dense(32, activation='relu')(state_input)
-    x = Dense(32, activation='relu')(x)
-    value_output = Dense(1)(x)
-    model = Model(inputs=state_input, outputs=value_output)
-    return model
 
-# Define the GAC agent
-class GACAgent:
-    def __init__(self, state_dim, action_dim):
-        self.state_dim = state_dim
-        self.action_dim = action_dim
-        self.actor_network = create_actor_network(state_dim, action_dim)
-        self.critic_network = create_critic_network(state_dim)
-        self.actor_optimizer = Adam(lr=0.001)
-        self.critic_optimizer = Adam(lr=0.001)
-    
-    def act(self, state):
-        action_prob = self.actor_network.predict(np.array([state]))[0]
-        action_dist = tfp.distributions.Bernoulli(probs=action_prob)
-        action = action_dist.sample()
-        return action.numpy()[0]
-    
-    def learn(self, state, action, reward, next_state, done):
-        # Compute TD error
-        td_target = reward + self.gamma * self.q_target(next_state, self.actor_target(next_state))
-        td_error = td_target - self.q_value(state, action)
+class Critic ( nn.Module ):
+	def __init__ ( self , state_dim ):
+		super ( Critic , self ).__init__ ()
+		self.fc1 = nn.Linear ( state_dim , 128 )
+		self.fc2 = nn.Linear ( 128 , 64 )
+		self.fc3 = nn.Linear ( 64 , 1 )
 
-        # Update Q function
-        q_loss = F.mse_loss(self.q_value(state, action), td_target)
-        self.q_optimizer.zero_grad()
-        q_loss.backward()
-        self.q_optimizer.step()
+	def forward ( self , state ):
+		x = F.relu ( self.fc1 ( state ) )
+		x = F.relu ( self.fc2 ( x ) )
+		x = self.fc3 ( x )
+		return x
 
-        # Update policy function
-        policy_loss = -self.q_value(state, self.actor(state)).mean()
-        self.actor_optimizer.zero_grad()
-        policy_loss.backward()
-        self.actor_optimizer.step()
 
-        # Update target networks
-        self.update_target_networks()
+class GACA_GBM:
+	def __init__ ( self , state_dim , action_dim , gamma = 0.99 , tau = 0.005 , lr = 0.001 ):
+		self.actor = Actor ( state_dim , action_dim )
+		self.actor_target = Actor ( state_dim , action_dim )
+		self.critic = Critic ( state_dim )
+		self.critic_target = Critic ( state_dim )
+		self.actor_optimizer = optim.Adam ( self.actor.parameters () , lr = lr )
+		self.critic_optimizer = optim.Adam ( self.critic.parameters () , lr = lr )
+		self.gamma = gamma
+		self.tau = tau
+		self.soft_update ( self.actor , self.actor_target , 1.0 )
+		self.soft_update ( self.critic , self.critic_target , 1.0 )
 
-        return td_error.detach().numpy()
-    
-                                              
-def main():
-    # Set the option parameters
-    S0 = 100   # Stock price
-    K = 100    # Strike price
-    T = 10     # Time to maturity (in years)
-    sigma = 0.2   # Volatility
-    r = 0.05   # Risk-free rate
-    
-    # Create the GAC agent
-    agent = GACAgent(state_dim, action_dim)
-    
-    # Train the agent
-    episode_reward = 0
-    state = S0
-    for t in range(1000):
-        action = agent.act(state)
-        reward = -bs_put(state, K, T, r, sigma)
-        
-        next_state = state - 1
-        if action == 1 or t == 999:
-            done = True
-        else:
-            done = False
-        
-        agent.learn(state, action, reward, next_state, done)
-        
-        state = next_state
-        episode_reward += reward
-        
-        if done:
-            print('Episode: {}, Reward: {:.2f}'.format(t+1, episode_reward))
-            break
-    
-    # Price the American ATM put option
-    option_price = bs_put(S0, K, T, r, sigma)
-    print('Option price (European): {:.2f}'.format(option_price))
-    
-    state = S0
-    while True:
-        action = agent.act(state)
-        if action == 1:
-            option_price = bs_put(state, K, T, r, sigma)
-            print('Option price (American): {:.2f}'.format(option_price))
-            break
-        else:
-            state -= 1
+	def act ( self , state , exploration_noise = 0.1 ):
+		state = torch.FloatTensor ( state )
+		action = self.actor ( state ).detach ().numpy ()
+		action += exploration_noise * np.random.randn ( self.actor.out_features )
+		action = np.clip ( action , -1.0 , 1.0 )
+		return action
+
+	def q_value ( self , state , action ):
+		state = torch.FloatTensor ( state )
+		action = torch.FloatTensor ( action )
+		q_value = self.critic ( torch.cat ( [ state , action ] , 1 ) )
+		return q_value
+
+	def q_target ( self , next_state , next_action ):
+		next_state = torch.FloatTensor ( next_state )
+		next_action = torch.FloatTensor ( next_action )
+		q_target = self.critic_target ( torch.cat ( [ next_state , next_action ] , 1 ) )
+		return q_target
+
+	def update_target_networks ( self ):
+		for target_param , param in zip ( self.actor_target.parameters () , self.actor.parameters () ):
+			target_param.data.copy_ ( self.tau * param.data + (1 - self.tau) * target_param.data )
+
+		for target_param , param in zip ( self.critic_target.parameters () , self.critic.parameters () ):
+			target_param.data.copy_ ( self.tau * param.data + (1 - self.tau) * target_param.data )
+
+	def soft_update ( self , net , target_net , tau ):
+		for target_param , param in zip ( target_net.parameters () , net.parameters () ):
+			target_param.data.copy_ ( tau * param.data + (1 - tau) * target_param.data )
+
+		def learn ( self , replay_buffer , iterations , batch_size = 64 , gamma = 0.99 , soft_tau = 1e-2 ):
+			for it in range ( iterations ):
+				# Sample replay buffer
+				state , action , reward , next_state , done = replay_buffer.sample ( batch_size )
+				state = torch.FloatTensor ( state ).to ( self.device )
+				next_state = torch.FloatTensor ( next_state ).to ( self.device )
+				action = torch.FloatTensor ( action ).to ( self.device )
+				reward = torch.FloatTensor ( reward ).to ( self.device )
+				done = torch.FloatTensor ( done ).to ( self.device )
+
+				# Compute the target Q value
+				target_Q = self.critic_target ( next_state , self.actor_target ( next_state ) )
+				target_Q = reward + ((1 - done) * gamma * target_Q).detach ()
+
+				# Get current Q estimate
+				current_Q = self.critic ( state , action )
+
+				# Compute critic loss
+				critic_loss = F.mse_loss ( current_Q , target_Q )
+
+				# Optimize the critic
+				self.critic_optimizer.zero_grad ()
+				critic_loss.backward ()
+				self.critic_optimizer.step ()
+
+				# Compute actor loss
+				actor_loss = -self.critic ( state , self.actor ( state ) ).mean ()
+
+				# Optimize the actor
+				self.actor_optimizer.zero_grad ()
+				actor_loss.backward ()
+				self.actor_optimizer.step ()
+
+				# Update the target networks
+				for param , target_param in zip ( self.critic.parameters () , self.critic_target.parameters () ):
+					target_param.data.copy_ ( soft_tau * param.data + (1 - soft_tau) * target_param.data )
+
+				for param , target_param in zip ( self.actor.parameters () , self.actor_target.parameters () ):
+					target_param.data.copy_ ( soft_tau * param.data + (1 - soft_tau) * target_param.data )
+
+
+def main ():
+	# Set the option parameters
+	S0 = 100  # Stock price
+	K = 100  # Strike price
+	T = 10  # Time to maturity (in years)
+	sigma = 0.2  # Volatility
+	r = 0.05  # Risk-free rate
+
+
+main ()
